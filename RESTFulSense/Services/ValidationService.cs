9f2b262df8dfae128458159cb3fdc2cd6892418a
zabanet <zakrafal@gmail.com>
2021-11-13 07:05:59 +0100

    added custom timeout - before moving to windows

--- RESTFulSense/Services/ValidationService.cs
+++ RESTFulSense/Services/ValidationService.cs
@@ -4,7 +4,6 @@
 // See License.txt in the project root for license information.
 // ---------------------------------------------------------------
 
-using System;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
